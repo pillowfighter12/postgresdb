8608fe74f7d798bda70ef4fdc32c4571dac8dcd2
neenzk61 <alisonqual1990@gmail.com>
2023-03-29 01:36:31 -0400

    update to code for updated column name

--- postgreslesson_4.sql
+++ postgreslesson_4.sql
@@ -7,11 +7,15 @@ CREATE TABLE sloths (
 );
 
 
-INSERT INTO sloths (name, email, age, created_at) VALUES ('Mt.Carmel', 'MtCaramel@gmail.com', 3346, CURRENT_TIMESTAMP);
+alter table sloths rename column name to username;
 
+
+INSERT INTO sloths (name, email, age, created_at) VALUES ('Mt.Carmel', 'MtCaramel@gmail.com', 3346, CURRENT_TIMESTAMP);
 
 
 update sloths set email = 'thisisnotarealemail@gmail.com' where id = 1;
 
 
 SELECT id, username, email FROM sloths WHERE age < 40;
+
+
